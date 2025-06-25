b7e50980707ed29251e54f061f172829df86955d
Matheus Jost <matheusfjost@outlook.com>
2025-06-25 20:51:10 -0300

    feat: boot spring project

---
+++ src/main/java/br/unisc/tuberculosis_rna/TuberculosisRnaApplication.java
@@ -0,0 +1,13 @@
+package br.unisc.tuberculosis_rna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuberculosisRnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuberculosisRnaApplication.class, args);
+	}
+
+}
